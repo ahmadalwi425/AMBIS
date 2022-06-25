50c30f800928bb772ea4e1e469770ef2f564c573
nargyanti <nargyanti@gmail.com>
2022-06-25 18:38:18 +0700

    Change button in profile

--- react-ambis/src/components/Profile.js
+++ react-ambis/src/components/Profile.js
@@ -1,17 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
-import { Button, Row, Col } from 'react-bootstrap';
 
 // Class Component Header 
 function Profile() {
     return (
         <div>
             <Link to="/createevent">
-                    <Button as={Col} style={{ background: "#2D1B6B", 
-                    borderRadius: "30px", 
-                    width: "310px"}}>
+                    <button className="btn btn-primary" style={{ background: "#2D1B6B", borderRadius: "30px",  width: "310px"}}>
                         Create an Event
-                        </Button>
+                    </button>
             </Link>
         </div>
     );
