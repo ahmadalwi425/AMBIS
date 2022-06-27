bfe96e8d85894ffbad1230dcf71d78138c4f4744
nargyanti <nargyanti@gmail.com>
2022-06-28 00:35:10 +0700

    Update show event by id with functional component

--- react-ambis/src/components/EventDetail.jsx
+++ react-ambis/src/components/EventDetail.jsx
@@ -1,50 +1,39 @@
-import React, { Component } from "react";
-import '../style.css';
-import firebase from '../Firebase';
+import { BrowserRouter as Router, Link, Route, Routes, useParams } from "react-router-dom";
+import firebase from "../Firebase";
+import React, { useState, useEffect } from 'react';
 
-class EventDetail extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            event: {},
-            key: ''
-        };
-    }
+const EventDetail = () => {
+    const params = useParams()    
+    const [event, setEvent] = useState({});
 
-    componentDidMount() {
-        const id = "HU2JWCHvw55fpap6pRr3";
-        const ref = firebase.firestore().collection('events').doc(id);
+    useEffect(() => {
+        const ref = firebase.firestore().collection('events').doc(params.id);        
         ref.get().then((doc) => {
             if (doc.exists) {
-                this.setState({
-                    event: doc.data(),
-                    key: doc.id,
-                    isLoading: false
-                });
+                setEvent(
+                    doc.data(),                    
+                );
             } else {
                 console.log("No such document!");
             }
-        });
-    }
+        })
+    }, []);    
 
-    render() {
-        return (
-            <div>
-                <img src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a" className="img-fluid" alt="Image Cover" style={{ maxHeight: "300px", objectFit: "cover", width: "100%" }} />
-                <div className="event px-5 py-4">
-                    <h1 className="event-title">{this.state.event.title}</h1>
-                    <p className="event-desc" style={{ textAlign: "justify" }}>{this.state.event.desc}</p>
-                    <div>
-                        {/* <span className="badge border border-dark rounded-pill fs-6 fw-normal text-dark" >{this.state.event.due_date}</span>
-                        <span className="badge border border-dark rounded-pill fs-6 mx-3 fw-normal  text-dark">{this.state.event.location}</span> */}
-                    </div>
-                    <div className="text-center">
-                        <a href={this.state.event.link} target="_blank"><button type="button" className="btn ambis-btn-primary w-25" style={{ height: "50px" }}>Apply Now!</button></a>                                         
-                    </div>
+    return (
+        <div>
+            <img src={event.thumbnail} className="img-fluid" alt="Image Cover" style={{ maxHeight: "300px", objectFit: "cover", width: "100%" }} />
+            <div className="event px-5 py-4">
+                <h1 className="event-title">{event.title}</h1>
+                <p className="event-desc" style={{ textAlign: "justify" }}>{event.desc}</p>
+                <div>
+                    {/* <span className="badge border border-dark rounded-pill fs-6 fw-normal text-dark" >{this.state.event.due_date}</span>
+                    <span className="badge border border-dark rounded-pill fs-6 mx-3 fw-normal  text-dark">{this.state.event.location}</span> */}
+                </div>
+                <div className="text-center">
+                    <a href={event.link} target="_blank"><button type="button" className="btn ambis-btn-primary w-25" style={{ height: "50px" }}>Apply Now!</button></a>                                         
                 </div>
             </div>
-        );
-    }
-}
-
+        </div>
+    );
+};
 export default EventDetail;
